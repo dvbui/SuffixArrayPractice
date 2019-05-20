54efe7d6038ed59696fb0da5973fb9089db4aa0b
dvbui <dvbui@wisc.edu>
2019-05-20 18:35:43 -0500

    Done with LCP array

--- Buzzwords.cpp
+++ Buzzwords.cpp
@@ -1,117 +1,157 @@
-//
-// Created by bui on 5/20/19.
-//
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 #include <vector>
 #define ii pair<int,int>
-
 using namespace std;
+const int maxn = 2000000;
+const int alphabet = 256;
+string s;
+string t;
+int n;
+int cnt[maxn],p[maxn],c[maxn];
+int cn[maxn],pn[maxn];
+int rnk[maxn],lcp[maxn],rlcp[maxn];
+int classes;
 
-/**
- * Adapted from https://cp-algorithms.com/string/suffix-array.html
- */
-class SuffixArray
+void solve()
 {
-private:
-    static const int alphabet = 256;
-    int n;
-    string s;
-    vector<int> array;
+    t="";
+    for (int i=0; i<s.size(); i++) {
+        if ('A' <= s[i] and s[i] <= 'Z')
+            t += s[i];
+    }
+    s=t;
+    s+='$';
+    n = s.size();
+    for (int i=0; i<alphabet; i++)
+        cnt[i] = 0;
 
-    void suffixArrayFirstPass(vector<int>& currentResult,vector<int>& currentClass,vector<int>& cnt)
+    for (int i=0; i<n; i++)
+        cnt[s[i]]++;
+    for (int i=1; i<alphabet; i++)
+        cnt[i]+=cnt[i-1];
+
+    for (int i=0; i<n; i++)
+        p[--cnt[s[i]]] = i;
+
+    classes = 1;
+    c[p[0]] = 0;
+
+    for (int i=1; i<n; i++)
+    {
+        if (s[p[i]]!=s[p[i-1]])
+            classes++;
+        c[p[i]] = classes - 1;
+    }
+
+
+    for (int h=1; h<n; h<<=1)
     {
         for (int i=0; i<n; i++)
-            cnt[s[i]]++;
-        for (int i=1; i<alphabet; i++)
-            cnt[i] = cnt[i-1];
-        for (int i=0; i<n; i++)
         {
-            cnt[s[i]]--;
-            currentResult[i] = cnt[s[i]];
+            pn[i] = p[i] - h;
+            if (pn[i]<0)
+                pn[i]+=n;
         }
-        int numberOfCurrentClass = 1;
-        currentClass[currentResult[0]] = 0;
+
+        /*for (int i=0; i<n; i++)
+            cout << pn[i] << ' ';
+        cout << '\n';*/
+
+        for (int i=0; i<classes; i++)
+            cnt[i] = 0;
+
+        for (int i=0; i<n; i++)
+            cnt[c[pn[i]]]++;
+
+
+        for (int i=1; i<classes; i++)
+            cnt[i]+=cnt[i-1];
+
+        //cout << cnt[classes-1] << '\n';
+
+        for (int i=n-1; i>=0; i--)
+            p[--cnt[c[pn[i]]]] = pn[i];
+
+        cn[p[0]] = 0;
+        classes = 1;
 
         for (int i=1; i<n; i++)
         {
-            if (s[currentResult[i]]!=s[currentResult[i-1]])
-                numberOfCurrentClass++;
-            currentClass[currentResult[i]] = numberOfCurrentClass--;
+            ii cur = ii(c[p[i]],c[(p[i]+h)%n]);
+            ii prev = ii(c[p[i-1]],c[(p[i-1]+h)%n]);
+
+            if (cur!=prev)
+                classes++;
+
+            cn[p[i]] = classes - 1;
         }
+
+
+        for (int i=0; i<n; i++)
+            c[i] = cn[i];
+
+        /*for (int i=0; i<n; i++)
+            cout << p[i] << ' ';
+        cout << '\n';
+        for (int i=0; i<n; i++)
+            cout << c[i] << ' ';
+        cout << '\n';
+        cout << classes << '\n';
+        cout << '\n';*/
     }
 
-    void suffixArrayNextPasses(vector<int>& currentResult,vector<int>& currentClass,vector<int>& cnt)
+
+    /*for (int i=0; i<n; i++)
+        cout << s.substr(p[i],s.size()-p[i]) << ' ' << p[i] << '\n';
+    cout << '\n';*/
+
+    for (int i=0; i<n; i++)
+        rnk[p[i]] = i;
+
+    int k = 0;
+    for (int i=0; i<n; i++)
     {
-        vector<int> newResult(n),newClass(n);
-        for (int h=1; h<n; h<<=1)
+        if (rnk[i] == n-1)
         {
-            for (int i=0; i<n; i++) {
-                newResult[i] = currentResult[i] - h;
-                if (newResult[i] < 0)
-                    newResult[i] += n;
-            }
-
-            cnt.clear();
-            cnt.resize(n);
-
-            for (int i=0; i<n; i++)
-                cnt[currentClass[newResult[i]]]++;
-
-            for (int i=0; i<n; i++)
-                cnt[i] += cnt[i-1];
-
-            for (int i=0; i<n; i++)
-            {
-                cnt[currentClass[newResult[i]]]--;
-                currentResult[cnt[currentClass[newResult[i]]]] = newResult[i];
-            }
-
-            newClass[currentResult[0]] = 0;
-            int numberOfNewClass = 1;
-
-            for (int i=1; i<n; i++)
-            {
-                ii cur = {currentClass[currentResult[i]],currentClass[(currentResult[i]+h)%n]};
-                ii prev = {currentClass[currentResult[i-1]],currentClass[(currentResult[i-1]+h)%n]};
-                if (cur!=prev)
-                    numberOfNewClass++;
-                newClass[currentResult[i]] = numberOfNewClass-1;
-            }
-
-            currentClass.swap(newClass);
+            k=0;
+            continue;
         }
-    }
-    void constructSuffixArray()
-    {
-        vector<int> currentResult(n),currentClass(n),cnt(max(n,alphabet),0);
-        suffixArrayFirstPass(currentResult,currentClass,cnt);
-        suffixArrayNextPasses(currentResult,currentClass,cnt);
-        array = currentResult;
-        array.erase(array.begin());
+
+        int j = p[rnk[i]+1];
+
+        while (j+k<n and i+k<n and s[i+k]==s[j+k])
+            k++;
+
+        lcp[rnk[i]] = k;
+        //cout << rnk[i] << ' ' << k << '\n';
+
+        //cout << s.substr(i,s.size()-i) << ' ' << s.substr(j,s.size()-j) << ' ' << rnk[i] << ' ' << lcp[rnk[i]] << '\n';
+
+        if (k)
+            k--;
     }
 
-public:
-    SuffixArray()
-    {
-        this->n = 0;
-        this->s = "";
+
+    /*for (int i=0; i<n; i++)
+        cout << s[p[rnk[i]]] << ' ' << lcp[i] << '\n';
+    cout << '\n';*/
+
+    for (int i=0; i+1<n; i++) {
+        cout << s.substr(p[i], s.size() - p[i]) << ' ' << s.substr(p[i + 1], s.size() - p[i + 1]) << ' ' << lcp[i]
+             << '\n';
     }
 
-    SuffixArray(string s)
-    {
-        this->n = s.size();
-        this->s = s;
-        constructSuffixArray();
-    }
 
-};
+}
 int main()
 {
     ios_base::sync_with_stdio(0);
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
-
+    while (getline(cin,s) and s!="")
+        solve();
 
     return 0;
 }
